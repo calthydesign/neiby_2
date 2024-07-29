e183e3556d7d2890f8032d17abd4fd880a941338
calthydesign <calthy.design@gmail.com>
2024-07-29 03:31:02 +0000

    ダッシュボード修正

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,33 @@
+<style>
+@keyframes slideIn {
+    from {
+        opacity: 0;
+        transform: translateY(20px); /* 下からスライドイン */
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+.slide-in {
+    opacity: 0; /* 初めは非表示 */
+    animation: slideIn 1s forwards; /* 1秒でスライドイン */
+}
+
+.delay-0 {
+    animation-delay: 0s; /* 最初のspan */
+}
+
+.delay-2000 {
+    animation-delay: 2s; /* 2秒後に表示 */
+}
+
+.delay-4000 {
+    animation-delay: 4s; /* 4秒後に表示 */
+}
+</style>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -14,28 +44,39 @@
             </div>
             
         <!--説明スライド-->
-        <div class="responsive-iframe-container" style="position: relative; max-width: 680px; width: 100%; height: auto; padding-top: 56.2500%;
-             padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-             border-radius: 8px; will-change: transform;">
-          <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-            src="https://www.canva.com/design/DAGKKp9FRpM/iPqdHlk_h3CE9QxptWkuRg/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-          </iframe>
+        <div class="my-4">
+            <p>アプリの説明は<a href="https://www.canva.com/design/DAGKKp9FRpM/XHM6Nq4_AWPrwo2EXMrlRg/view?utm_content=DAGKKp9FRpM&utm_campaign=designshare&utm_medium=link&utm_source=editor" class="text-blue-500 underline">こちら</a></p>
         </div>
-        <a href="https://www.canva.com/design/DAGKKp9FRpM/iPqdHlk_h3CE9QxptWkuRg/view?utm_content=DAGKKp9FRpM&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">アプリの使い方をご覧いただき、診断から開始してください。</a>
         
-            <div class="mt-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <!--よもぎAIBOT-->
+        <div class="m-8 p-4 bg-white rounded-xl">
+            <p>こんにちは！よもぎ先生です。<br>
+                <span class="slide-in delay-0">このアプリは、東洋医学における「未病」改善を目指すサービスを提供しています。<br></span>
+                <span class="slide-in delay-2000">「病院に行っても問題ないけどずっと体調不良がある」「体質なのか不調があるけど諦めている」そんな方をサポートしていきます！<br></span>
+                <span class="slide-in delay-4000">はじめに、あなたはこれからどうしたいか教えてもらえますか？</span>
+            </p>
+        </div>
+        
+        <div class="mt-6 p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <h2>いますぐ専門家に相談したい</h2>
+            <p>→<a href="https://www.google.co.jp/maps/search/%E9%8D%BC%E7%81%B8%E5%B8%AB/" class="text-blue-500 underline">お近くの鍼灸師検索</a></p>
+            <p>→<a href="#" class="text-blue-500 underline">よもぎ先生にオンライン相談</a></p>
+        </div>
+        
+        <div class="mt-6 p-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <h2>自分の体調について知りたい、自分で改善してみたい</h2>
+                <p>→気血水の診断をして、体質に合った習慣を実践してみましょう！</p>
                 <div class="p-6 text-gray-900">
                     <a href="{{ route('diagnoses.index') }}" class="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                         {{ __('診断する') }}
                     </a>
                 </div>
-            <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900">
                     <a href="{{ route('posts.index') }}" class="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                         {{ __('体質に合った習慣をみる') }}
                     </a>
                     <p class="text-sm mt-4">※診断をしていない場合は診断画面に移動します。</p>
-            </div>
-            </div>
+                </div>
         </div>
     </div>
 </x-app-layout>
